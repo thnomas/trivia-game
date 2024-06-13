3526f489c0cdf683a899ec2b4d7dd694f3a2df3c
thnomas <thombot43@gmail.com>
2024-06-13 14:28:20 +0100

    added type annotations

--- game.py
+++ game.py
@@ -10,13 +10,13 @@ class Game:
         self.points_to_win = points_to_win
 
     @staticmethod
-    def check_answer(user_answer, correct_answer):
+    def check_answer(user_answer: str, correct_answer: str) -> bool:
         if user_answer == correct_answer:
             return True
         else:
             return False
 
-    def play_game(self):
+    def play_game(self) -> None:
         print(f"First to {self.points_to_win} points wins")
         print("-- LETS PLAY --")
         while True:
@@ -25,7 +25,7 @@ class Game:
             roll = current_player.roll_dice(2)
             print(f"{roll[1]}...You rolled a {roll[0]}")
 
-            if roll[0] + self.score[self.current_player_index] > self.points_to_win:
+            if roll[0] + self.score[self.current_player_index] >= self.points_to_win:
                 print("** THIS WILL WIN THE GAME! **")
 
             q = random.choice(questions)
